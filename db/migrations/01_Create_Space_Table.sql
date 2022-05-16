48306803549b78f61727fe2a77e6925656c13343
sjtinsley <simonjtinsley@gmail.com>
2022-05-16 15:59:58 +0100

    Failing feature test

---
+++ db/migrations/01_Create_Space_Table.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE chuckle_hotel;
+
+-- In database chuckle_hotel
+CREATE TABLE spaces (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, description TEXT NOT NULL, price MONEY NOT NULL);
