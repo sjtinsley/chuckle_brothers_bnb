17aa82346b3bb6773119da2e8ceb3479f12b7493
sjtinsley <simonjtinsley@gmail.com>
2022-05-17 11:59:25 +0100

    Fix DB migration files

--- db/migrations/01_Create_Space_Table.sql
+++ db/migrations/01_Create_Space_Table.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE chuckle_hotel;
 
 -- In database chuckle_hotel
-CREATE TABLE spaces (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, description TEXT NOT NULL, price MONEY NOT NULL);
+CREATE TABLE spaces (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, description TEXT NOT NULL, price NUMERIC NOT NULL);
 
 SET lc_monetary to 'en_GB';
