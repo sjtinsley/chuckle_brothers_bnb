1856741b27141ca8acab5de5c09104e5ddb77d05
sjtinsley <simonjtinsley@gmail.com>
2022-05-17 15:33:41 +0100

    Added password hashing

--- lib/user.rb
+++ lib/user.rb
@@ -1,15 +1,23 @@
 require 'pg'
+require 'bcrypt'
 
 class User
-  attr_reader :id, :username, :email
+  attr_reader :id, :username, :email, :pass_hash
 
-  def initialize(id:, username:, email:)
+  def initialize(id:, username:, email:, pass_hash:)
     @id = id
     @username = username
     @email = email
+    @pass_hash = pass_hash
   end
 
   def self.create(username:, email:, password:)
+   
+    encrypted_pass = BCrypt::Password.create(password)
+    
+    # p password
+    # p encrypted_pass
+
     if ENV['RACK_ENV'] == 'test'
       connection = PG.connect(dbname: 'chuckle_hotel_test')
     else
@@ -17,13 +25,14 @@ class User
     end
 
     result = connection.exec_params("INSERT INTO users (username, email, password)
-    VALUES($1, $2, $3) returning id, username, email;",
-    [username, email, password])
+    VALUES($1, $2, $3) returning id, username, email, password;",
+    [username, email, encrypted_pass])
 
     User.new(
       id: result[0]['id'],
       username: result[0]['username'], 
-      email: result[0]['email']
+      email: result[0]['email'],
+      pass_hash: result[0]['password']
     )
   end
 
@@ -36,7 +45,8 @@ class User
 
     result = connection.exec("SELECT * FROM users;")
     result.map do |user|
-      User.new(id: user['id'], username: user['username'], email: user['email'])
+      User.new(id: user['id'], username: user['username'], email: user['email'], pass_hash:user['password'])
     end
   end
+  
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,6 +5,12 @@ describe User do
     it 'should add a user to the database' do
       expect(User.create(username: 'test', email: 'test@email.com', password: "bobbychuckle")).to be_an_instance_of(User).and have_attributes(username: 'test', email: 'test@email.com')
     end
+
+    it 'hashes the password using BCrypt' do
+      a = User.create(username: 'test', email: 'test@email.com', password: "bobbychuckle")
+      expect(a.pass_hash.length).to eq 60
+    end
+
   end
 
   describe '.all' do
