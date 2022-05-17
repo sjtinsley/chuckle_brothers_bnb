f20f04cbaa9e50b624f89fbdc429b05e39e584b2
George Dainton <gcdainton94@gmail.com>
2022-05-17 11:13:23 +0100

    Updated view_spaces_spec to use .create method

--- spec/features/view_spaces_spec.rb
+++ spec/features/view_spaces_spec.rb
@@ -2,8 +2,11 @@ require 'capybara/rspec'
 
 feature "view spaces" do
   scenario "user can see the available spaces" do
+    Space.create(name: 'name', description: 'description', price: 4)
     visit("/spaces")
-
-    expect(page).to have_content("Available Spaces")
+    
+    expect(page).to have_content('name')
+   
+    
   end
 end
