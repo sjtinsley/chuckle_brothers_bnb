d28243a5782578ea7631f4b1eba27c1751addde7
Luke Storey <lukemstorey@gmail.com>
2022-05-17 14:30:18 +0100

    Create space specific ID page

--- spec/features/view_a_space_spec.rb
+++ spec/features/view_a_space_spec.rb
@@ -1,7 +1,10 @@
-xfeature 'view a space' do
+feature 'view a space' do
   scenario 'user can view a selected space' do
     space = Space.create(name: 'name', description: 'description', price: 4)
-    visit("/spaces/#{space.id}")
+    
+    visit '/spaces'
+
+    first('.space').click
 
     expect(page).to have_content('name')
     expect(page).to have_content('description')
