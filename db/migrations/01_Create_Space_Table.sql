5f4c9bde89b7ee42eadaa26ddd3efd3fd5a12d7e
sjtinsley <45486136+sjtinsley@users.noreply.github.com>
2022-05-17 16:42:13 +0100

    Delete 01_Create_Space_Table.sql
    I got 99 problems and merging is one

--- db/migrations/01_Create_Space_Table.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE chuckle_hotel;
-
--- In database chuckle_hotel
-CREATE TABLE spaces (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, description TEXT NOT NULL, price NUMERIC NOT NULL);
